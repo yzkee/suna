078f22919293ffeb393880ddff99428291cd02fd
Vukasin <vukasinkubet@gmail.com>
2025-11-06 20:54:59 +0100

    feat: add back auth

--- backend/supabase/migrations/20251103100000_fix_anon_access_for_public_shares.sql
+++
@@ -1,8 +0,0 @@
--- Fix anonymous access to public shared threads
--- The 20250901030240_security.sql migration revoked SELECT from anon
--- But we need anon to SELECT these tables (RLS policies will protect private data)
-
-GRANT SELECT ON TABLE threads TO anon;
-GRANT SELECT ON TABLE messages TO anon;
-GRANT SELECT ON TABLE projects TO anon;
-GRANT SELECT ON TABLE agent_runs TO anon;
