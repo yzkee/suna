f17e5e405999b5b46c4886b5f473b2792563bf50
Saumya <saumyadas2017@gmail.com>
2025-11-27 20:25:25 +0530

    delete inspect files

--- backend/inspect_novu_to1.py
+++
@@ -1,5 +0,0 @@
-import novu_py
-
-To1 = novu_py.models.triggereventrequestdto.To1
-print(f"To1 value: {To1.__value__}")
-
--- backend/inspect_novu_to_value.py
+++
@@ -1,5 +0,0 @@
-import novu_py
-
-To = novu_py.To
-print(f"To value: {To.__value__}")
-
