dce4d35194fee57b510faa99212de8e6275180d7
Saumya <saumyadas2017@gmail.com>
2025-12-24 16:31:57 +0530

    fix tag manager

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -100,15 +100,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={`${roobert.variable} ${roobertMono.variable}`}>
       <head>
-        {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PKFG3JCX');`}
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PKFG3JCX');`}
         </Script>
-        {/* End Google Tag Manager */}
+        
         {/* Preload critical fonts for faster FCP - local fonts need crossOrigin for CORS */}
         <link
           rel="preload"
@@ -220,7 +219,6 @@ export default function RootLayout({
       </head>
 
       <body className="antialiased font-sans bg-background">
-        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-PKFG3JCX"
