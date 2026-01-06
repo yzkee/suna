7dbf6468fbaa3010d917b3e27cd850e83b10c61f
pablocpz <pablocoboperez07@gmail.com>
2026-01-06 23:45:19 +0100

    fix: add react-native-worklets/plugin to babel config for expensify-common worklets support

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -3,6 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo', 'nativewind/babel'],
     plugins: [
+      'react-native-worklets/plugin',
       'react-native-reanimated/plugin',
     ],
   };
