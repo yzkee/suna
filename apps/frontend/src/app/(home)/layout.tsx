adcc4a0330e5dd6c0d5ceb02ea420dd2f4c94f5e
emrkarata <emrekaratas@mail.de>
2026-01-20 14:12:47 +0100

    Restore dashboard-style landing page

--- apps/frontend/src/app/(home)/layout.tsx
+++ apps/frontend/src/app/(home)/layout.tsx
@@ -8,7 +8,7 @@ export default function HomeLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full relative">
+    <div className="w-full min-h-dvh relative">
       <Navbar />
       {children}
     </div>
