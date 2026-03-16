bb40536543fcb8b9755c4532dad5a307e34228f2
Vukasin <vukasinkubet@gmail.com>
2026-03-16 20:05:28 +0100

    release: v0.8.9

--- scripts/get-kortix.sh
+++ scripts/get-kortix.sh
@@ -28,7 +28,7 @@ fatal()   { error "$*"; exit 1; }
 
 # ─── Config ──────────────────────────────────────────────────────────────────
 INSTALL_DIR="${KORTIX_HOME:-$HOME/.kortix}"
-DEFAULT_KORTIX_VERSION="0.8.8"
+DEFAULT_KORTIX_VERSION="0.8.9"
 KORTIX_VERSION="${KORTIX_VERSION:-$DEFAULT_KORTIX_VERSION}"
 KORTIX_LOCAL_IMAGES="${KORTIX_LOCAL_IMAGES:-0}"
 KORTIX_LOCAL_TAG="${KORTIX_LOCAL_TAG:-latest}"
