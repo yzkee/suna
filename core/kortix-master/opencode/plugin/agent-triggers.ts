6d7b49b8fff51bc0a8d91c27cf096d7cea0c7894
marko-kraemer <markokraemer.mail@gmail.com>
2026-04-03 21:59:07 +0200

    cleanup: remove all old plugin dirs replaced by kortix-system

--- core/kortix-master/opencode/plugin/agent-triggers.ts
+++
@@ -1,8 +0,0 @@
-import { createTriggersPlugin } from "../../triggers/src/index"
-
-export default createTriggersPlugin({
-  directory: "/workspace",
-  webhookHost: "0.0.0.0",
-  webhookPort: 8099,
-  publicBaseUrl: process.env.SANDBOX_PUBLIC_URL || "http://localhost:8000",
-})
